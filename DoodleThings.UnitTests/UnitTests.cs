63f2bbba2fb3041589c83bd81cc1bbede68ac03a
unknown <mgirgin@MG-VDEV.redmond.corp.microsoft.com>
2013-07-08 22:14:05 -0700

    Initial commit: SPA template

---
+++ DoodleThings.UnitTests/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DoodleThings.UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void Basic_noop_test()
+        {
+        }
+    }
+}
