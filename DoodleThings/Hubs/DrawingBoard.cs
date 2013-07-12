5ed77e43e05fde2007539100a3dda00937ff74c8
Abhishek Nanda <abnanda@microsoft.com>
2013-07-11 17:37:52 -0700

    WIP

--- DoodleThings/Hubs/DrawingBoard.cs
+++ DoodleThings/Hubs/DrawingBoard.cs
@@ -13,38 +13,31 @@ public class DrawingBoard : Hub
 {
     // Map of connection id to connection id
     private static readonly ConcurrentDictionary<string, string> _games = new ConcurrentDictionary<string, string>();
+    private static readonly UserInfoController _userInfoController;
+
     public override Task OnConnected()
-    {        
+    {
         string name = Context.User.Identity.Name;
 
         Clients.Caller.Drawer = true;
         Clients.Caller.updateState();
 
-        //string name = Context.User.Identity.Name;
-        //    // Get User id from session cookie
-    //    Cookie userNameCookie;
+        // Update the new user's values
+        Clients.Caller.userName = name;
+        Clients.Caller.Drawer = true;
+        // Update the Game table with who is the drawer and who is the guesser
+        _userInfoController.UpdateConnectionId(name, Context.ConnectionId);
 
-    //    if (!Context.RequestCookies.TryGetValue("userName", out userNameCookie))
-    //    {
-    //        throw new InvalidOperationException("User Name missing");
-    //    }
+        UserInfo player2 = _userInfoController.GetRandomAvailablePlayer(name);
+        Clients.Client(player2.UserInfoId).Drawer = false;
 
-    //    // Update the new user's values
-    //    Clients.Caller.userName = userNameCookie.Value;
-    //    Clients.Caller.Drawer = true;
-    //    // Update the Game table with who is the drawer and who is the guesser
-    //    _userInfoController.UpdateConnectionId(userNameCookie.Value, Context.ConnectionId);
+        if (player2 != null)
+        {
+            // We are assuming that the second user here is the drawer
+            _games.TryAdd(Context.ConnectionId, player2.UserInfoId);
+        }
 
-    //    UserInfo player2 = _userInfoController.GetRandomAvailablePlayer(userNameCookie.Value);
-    //    Clients.Client(player2.UserInfoId).Drawer = false;
-
-        //if (player2 != null)
-    //    {
-    //        // We are assuming that the second user here is the drawer
-    //        _games.TryAdd(Context.ConnectionId, player2.UserInfoId);
-    //    }
-
-    //    // send a message to both the clients so that the UI can be updated
+        // send a message to both the clients so that the UI can be updated
 
         return base.OnConnected();
     }
@@ -52,7 +45,7 @@ public class DrawingBoard : Hub
     public Task BroadcastPoint(float x, float y)
     {
         string connectionId;
-   
+
         if (_games.TryGetValue(Context.ConnectionId, out connectionId))
         {
             return Clients.Client(connectionId).drawPoint(x, y, Clients.Caller.color);
